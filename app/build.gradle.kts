42119cb03d40d64f625fb03f2182a7b4d90d60f9
Weidong Guo <weidong.guo12@gmail.com>
2025-11-20 12:57:13 -0500

    Added navigation bar for child and parent (activity + layout) and registered it in Manifest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,9 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.demoapp"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.demoapp"
@@ -38,9 +36,11 @@ android {
 
 dependencies {
     implementation(libs.appcompat)
-    implementation(libs.material)
     implementation(libs.activity)
-    implementation(libs.constraintlayout)
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.0")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
