2ef442873471a485da39d4f1fc9fc15c8613eac5
cindyweng672 <cindyweng236@gmail.com>
2025-11-22 21:32:44 -0500

    Revert "chore: WIP before commit"
    This reverts commit adfb1d8c79d2b0c9794e7c6ba339b6ec0a92de7d.

--- app/src/main/java/com/example/demoapp/MainActivity.java
+++ app/src/main/java/com/example/demoapp/MainActivity.java
@@ -2,54 +2,14 @@ package com.example.demoapp;
 
 import android.os.Bundle;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-
-import androidx.recyclerview.widget.RecyclerView;
-import java.util.ArrayList;
-import com.example.demoapp.card_view.CardAdapter;
-import com.example.demoapp.card_view.CardItem;
-
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
-    private CardAdapter adapter;
-    private ArrayList<CardItem> cardList;
-
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); // your layout with FrameLayout
-
-        // Only add fragment if this is the first creation
-        if (savedInstanceState == null) {
-            //testing summary calendar
-            /*SummaryCalendarFragment calendarFragment = new SummaryCalendarFragment();
-            FragmentManager fragmentManager = getSupportFragmentManager();
-            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.add(R.id.fragment_container, calendarFragment);
-            fragmentTransaction.commit();*/
-            //testing display calendar entries
-            //DailyEntryDisplayScreen calendarFragment = new DailyEntryDisplayScreen();
-            //testing filter screen
-            FilterEntriesScreen filterFragment = new FilterEntriesScreen();
-            FragmentManager fragmentManager = getSupportFragmentManager();
-            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.add(R.id.fragment_container, filterFragment);
-            fragmentTransaction.commit();
-            //}
-        }
-
-        /*if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.fragment_container, new ProviderHomeScreen())
-                    .commit();
-        }*/
-    }
-}
-
-    /*
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,4 +20,5 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }*/
+    }
+}
