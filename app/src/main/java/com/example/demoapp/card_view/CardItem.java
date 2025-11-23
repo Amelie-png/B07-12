496fcbbf65e32ed6c911cb93adc43e18d29d52ea
cindyweng672 <cindyweng236@gmail.com>
2025-11-22 21:31:13 -0500

    feat: add username of child, parent, providers to associated CardItem

--- app/src/main/java/com/example/demoapp/card_view/CardItem.java
+++ app/src/main/java/com/example/demoapp/card_view/CardItem.java
@@ -1,14 +1,22 @@
 package com.example.demoapp.card_view;
 
+import java.util.ArrayList;
 public class CardItem {
     String name;
     int profilePic;
     int profileBackground;
 
-    public CardItem(String name, int profilePic, int profileBackground) {
+    String childUserName;
+    String parentUserName;
+    ArrayList<String> providerUserNames;
+
+    public CardItem(String name, int profilePic, int profileBackground, String childID, String parentID, ArrayList<String> providerIDs) {
         this.name = name;
         this.profilePic = profilePic;
         this.profileBackground = profileBackground;
     }
 
+    public void addProvider(String username){
+        providerUserNames.add(username);
+    }
 }
