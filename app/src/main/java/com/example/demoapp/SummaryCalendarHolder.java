c5a397c7fb5e2341de3efc7ed8395ce21951de38
cindyweng672 <cindyweng236@gmail.com>
2025-11-24 04:48:15 -0500

    feat: add parent add symptom screen

---
+++ app/src/main/java/com/example/demoapp/SummaryCalendarHolder.java
@@ -0,0 +1,36 @@
+package com.example.demoapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class SummaryCalendarHolder extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_parent_add_symptom_screen, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        if (savedInstanceState == null) {
+            SummaryCalendarFragment calendarFragment = new SummaryCalendarFragment();
+            getChildFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.display_browse_history_container, calendarFragment)
+                    .commit();
+        }
+    }
+}
